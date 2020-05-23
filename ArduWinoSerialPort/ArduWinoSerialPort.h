9fe58da3fea75b7d061f61bfd37c734342bb75f9
Petar Georgiev <pgeorgiev@codingburgas.bg>
2020-05-23 20:04:20 +0300

    Add project files.

---
+++ ArduWinoSerialPort/ArduWinoSerialPort.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+
+#define ARDUINO_WAIT_TIME 2000
+
+class ArduWinoSerialPort
+{
+private:
+    HANDLE hSerial;
+    bool connected;
+    COMSTAT status{0};
+    DWORD errors = 0;
+public:
+    ArduWinoSerialPort(const char* portName);
+    //Close the connection
+    ~ArduWinoSerialPort();
+    int ReadData(char* buffer, unsigned int nbChar);
+    bool WriteData(const char* buffer, unsigned int nbChar);
+    bool IsConnected();
+};
